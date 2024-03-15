ba55e5ae966de0a8d946bb667e0f93937acc8f3d
Daniel Pasion <dnpaxion@gmail.com>
2024-03-14 23:45:21 -0700

    Added all majors into one large json file

--- data_cleaning.py
+++ data_cleaning.py
@@ -8,7 +8,6 @@ import re
 df = pd.read_csv('Course_Prereqs/2023-2024_Courses.csv')
 
 #Parsing the data to get the list of all courses + all the major department acronyms
-
 #Arrays + Dictionary Declerations
 course_and_prereqs = {} #Ex: {'CS 1738': [CS 0069]}
 list_of_courses = [] #Ex: CS 1738
@@ -60,35 +59,54 @@ for term in df.values:
 
 
 ################################################################Preparing the major courses into a csv file#############################################
-                    
-#Reading the required courses file
-new_file = pd.read_csv("RequiredCourses/23-24RequiredCourses/77Mathematic.csv") #This will be any major you want
-courses_required = []
+import os
+# assign directory
+directory = 'RequiredCourses/23-24RequiredCourses'
+
+all_majors_json = {}
+# iterate over files in
+# that directory
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f):
+        #Reading the required courses file
+        new_file = pd.read_csv(f,encoding='unicode_escape') #This will be any major you want
+        courses_required = []
 
 
-#Appending the csv file data to an arrary
-for term in new_file.values:
-    courses_required.append(term[0])
+        #Appending the csv file data to an arrary
+        major = None
+        for title in new_file:
+            major = title
+        for term in new_file.values:
+            courses_required.append(term[0])
 
-#Removing this course that is in every major
-courses_required.remove('CPU 3003')
-finaljson = []
+        #Removing this course that is in every major
+        courses_required.remove('CPU 3003')
+        finaljson = []
 
-#Only including courses in the prereqs that are major required
-for course in courses_required:
-    former_prereqs = course_and_prereqs[course]
-    updated_prereqs = []
-    for term in former_prereqs:
-        if term in courses_required:
-            updated_prereqs.append(term)
-    finaljson.append({
-        "courseID": course,
-        "PreReqs": updated_prereqs
-    })
+        #Only including courses in the prereqs that are major required
+        for course in courses_required:
+            try:
+                former_prereqs = course_and_prereqs[course]
+                updated_prereqs = []
+                for term in former_prereqs:
+                    if term in courses_required:
+                        updated_prereqs.append(term)
+                finaljson.append({
+                    "courseID": course,
+                    "PreReqs": updated_prereqs
+                })
+            except:
+                print("Course thrown out: " + course)
+        
+        #Adding the major to the list of json
+        all_majors_json[major] = finaljson
 
 file_path = "course.json"
 
 # Write the data to the JSON file
 with open(file_path, "w") as json_file:
-    json.dump(finaljson, json_file)
+    json.dump(all_majors_json, json_file)
 
