a1895dc346672c1a4487c27eee8a9d32ef3de79b
Daniel Pasion <dnpaxion@gmail.com>
2024-03-22 04:45:02 -0700

    Created functional search bar, typing does not work at the moment

--- FlowChart/src/App.jsx
+++ FlowChart/src/App.jsx
@@ -1,7 +1,7 @@
 import ELK from "elkjs/lib/elk.bundled.js";
-import React, { useCallback, useLayoutEffect } from "react";
+import React, { useCallback, useLayoutEffect, useState, useEffect } from "react";
 import axios from 'axios'
-import "./styles.css"
+import "./styles/styles.css"
 import ReactFlow, {
   ReactFlowProvider,
   addEdge,
@@ -12,137 +12,45 @@ import ReactFlow, {
   useReactFlow,
   SmoothStepEdge,
 } from "reactflow";
-
 import "reactflow/dist/style.css";
-const elk = new ELK();
-//const fs = require("fs");
+import {prereqs} from "./components/prereqjson.jsx"
+import {allmajors, preandcoreqs} from "./components/preandcoreqjson.jsx"
+import SearchableDropdown from "./components/SearchableDropdown.jsx"
+import { LayoutFlow } from "./components/LayoutFlow.jsx";
 
-// Elk has a *huge* amount of options to configure. To see everything you can
-// tweak check out:
-//
-// - https://www.eclipse.org/elk/reference/algorithms.html
-// - https://www.eclipse.org/elk/reference/options.html
-const elkOptions = {
-  "elk.algorithm": "layered",
-  "elk.layered.spacing.nodeNodeBetweenLayers": "250",
-  "elk.spacing.nodeNode": "10",
-};
 
-const getLayoutedElements = (nodes, edges, options = {}) => {
-  const isHorizontal = options?.["elk.direction"] === "RIGHT";
-  const graph = {
-    id: "root",
-    layoutOptions: options,
-    children: nodes.map((node) => ({
-      ...node,
-      // Adjust the target and source handle positions based on the layout
-      // direction.
-      // targetPosition: isHorizontal ? "left" : "top",
-      // sourcePosition: isHorizontal ? "right" : "bottom",
+// export default () => (
+//   <ReactFlowProvider>
+//     <LayoutFlow major = "Computer Science, B.S.: 120 units"/>
+//   </ReactFlowProvider>
+// );
 
-      // Hardcode a width and height for elk to use when layouting.
-      width: 150,
-      height: 50,
-    })),
-    edges: edges,
-  };
+function App() {
+  //Used to set the values of the dropdown menu
+  let [major, setMajor] = useState("Computer Science, B.S.: 120 units");
+  
+  //Used to get all majors and store into options section of SearchableDropdown
+  const list_of_majors = allmajors();
 
-  return elk
-    .layout(graph)
-    .then((layoutedGraph) => ({
-      nodes: layoutedGraph.children.map((node) => ({
-        ...node,
-        // React Flow expects a position property on the node instead of `x`
-        // and `y` fields.
-        position: { x: node.x, y: node.y },
-      })),
-
-      edges: layoutedGraph.edges,
-    }))
-    .catch(console.error);
-};
-
-function LayoutFlow() {
-  const jsonData = jsonMajors['"Aerospace Engineering, B.S.: 127 units"']
-  console.log(jsonData)
-  const initialNodes = [];
-  const initialEdges = [];
-  // console.log(const course[0] in jsonData)
-  // for (const a in jsonData ) {
-  //   console.log(jsonData[a])
-  // }
-  // jsonData.forEach((course) => {
-  jsonData.forEach((course) => {
-    const nodeId = course.courseID;
-    const nodeData = { label: course.courseID };
-
-    // Create a node
-    initialNodes.push({
-      id: nodeId,
-      data: nodeData,
-    });
-
-    // Create edges for prerequisites
-    course.PreReqs.forEach((prereq) => {
-      const edgeId = `${prereq}-${nodeId}`;
-      initialEdges.push({
-        id: edgeId,
-        source: prereq,
-        target: nodeId,
-        // type: 'smoothstep',
-        markerEnd: {
-          type: MarkerType.ArrowClosed,
-        },
-        style: {
-          strokeWidth: "2px",
-        },
-      });
-    });
-  });
-
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const { fitView } = useReactFlow();
-
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    []
-  );
-  const onLayout = useCallback(
-    ({ direction, useInitialNodes = false }) => {
-      const opts = { "elk.direction": direction, ...elkOptions };
-
-      getLayoutedElements(initialNodes, initialEdges, opts).then(
-        ({ nodes: layoutedNodes, edges: layoutedEdges }) => {
-          setNodes(layoutedNodes);
-          setEdges(layoutedEdges);
-
-          window.requestAnimationFrame(() => fitView());
-        }
-      );
-    },
-    [nodes, edges]
-  );
-
-  // Calculate the initial layout on mount.
-  useLayoutEffect(() => {
-    onLayout({ direction: "DOWN", useInitialNodes: true });
-  }, []);
+  //Function used to update the major when clicked on the search bar
+  function changeMajor(major) {
+    setMajor(major)
+  }
 
   return (
-    <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      onConnect={onConnect}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      fitView
-    ></ReactFlow>
-  );
+    <>
+      <SearchableDropdown
+            options={list_of_majors}
+            label="major"
+            id="id"
+            selectedVal={major}
+            handleChange={(val) => changeMajor(val)}
+      />
+      <ReactFlowProvider>
+        <LayoutFlow major = {major}/>
+      </ReactFlowProvider>
+    </>
+  )
 }
 
-export default () => (
-  <ReactFlowProvider>
-    <LayoutFlow />
-  </ReactFlowProvider>
-);
+export default App
---
+++ FlowChart/src/components/LayoutFlow.jsx
@@ -0,0 +1,134 @@
+import ELK from "elkjs/lib/elk.bundled.js";
+import React, { useCallback, useLayoutEffect, useState, useEffect } from "react";
+import axios from 'axios'
+import "../styles/styles.css"
+import ReactFlow, {
+  ReactFlowProvider,
+  addEdge,
+  MarkerType,
+  Panel,
+  useNodesState,
+  useEdgesState,
+  useReactFlow,
+  SmoothStepEdge,
+} from "reactflow";
+import "reactflow/dist/style.css";
+import {prereqs} from "../components/prereqjson.jsx"
+import {allmajors, preandcoreqs} from "../components/preandcoreqjson.jsx"
+import SearchableDropdown from "../components/SearchableDropdown.jsx"
+
+const elk = new ELK();
+//const fs = require("fs");
+
+// Elk has a *huge* amount of options to configure. To see everything you can
+// tweak check out:
+//
+// - https://www.eclipse.org/elk/reference/algorithms.html
+// - https://www.eclipse.org/elk/reference/options.html
+const elkOptions = {
+  "elk.algorithm": "layered",
+  "elk.layered.spacing.nodeNodeBetweenLayers": "250",
+  "elk.spacing.nodeNode": "10",
+};
+
+const getLayoutedElements = (nodes, edges, options = {}) => {
+  const isHorizontal = options?.["elk.direction"] === "RIGHT";
+  const graph = {
+    id: "root",
+    layoutOptions: options,
+    children: nodes.map((node) => ({
+      ...node,
+      // Adjust the target and source handle positions based on the layout
+      // direction.
+      // targetPosition: isHorizontal ? "left" : "top",
+      // sourcePosition: isHorizontal ? "right" : "bottom",
+
+      // Hardcode a width and height for elk to use when layouting.
+      width: 150,
+      height: 50,
+    })),
+    edges: edges,
+  };
+
+  return elk
+    .layout(graph)
+    .then((layoutedGraph) => ({
+      nodes: layoutedGraph.children.map((node) => ({
+        ...node,
+        // React Flow expects a position property on the node instead of `x`
+        // and `y` fields.
+        position: { x: node.x, y: node.y },
+      })),
+
+      edges: layoutedGraph.edges,
+    }))
+    .catch(console.error);
+};
+
+    export const LayoutFlow = (props) => {
+    
+        const [nodes, setNodes, onNodesChange] = useNodesState([]);
+        const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+        const { fitView } = useReactFlow();
+        
+        useEffect(() => {
+            const jsonData = prereqs(props.major);
+            const initialNodes = [];
+            const initialEdges = [];
+            
+            jsonData.forEach((course) => {
+                const nodeId = course.courseID;
+                const nodeData = { label: course.courseID };
+                initialNodes.push({
+                    id: nodeId,
+                    data: nodeData,
+                });
+    
+                course.PreReqs.forEach((prereq) => {
+                    const edgeId = `${prereq}-${nodeId}`;
+                    initialEdges.push({
+                        id: edgeId,
+                        source: prereq,
+                        target: nodeId,
+                        markerEnd: {
+                            type: MarkerType.ArrowClosed,
+                        },
+                        style: {
+                            strokeWidth: "2px",
+                        },
+                    });
+                });
+            });
+    
+            const onLayout = ({ direction }) => {
+                const opts = { "elk.direction": direction, ...elkOptions };
+    
+                getLayoutedElements(initialNodes, initialEdges, opts).then(
+                    ({ nodes: layoutedNodes, edges: layoutedEdges }) => {
+                        setNodes(layoutedNodes);
+                        setEdges(layoutedEdges);
+                        fitView();
+                    }
+                );
+            };
+    
+            onLayout({ direction: "DOWN" });
+    
+        }, [props.major, setNodes, setEdges, fitView]);
+    
+        const onConnect = useCallback(
+            (params) => setEdges((eds) => addEdge(params, eds)),
+            []
+        );
+    
+        return (
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onConnect={onConnect}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                fitView
+            />
+        );
+    };
